66eb7e662efb47d1aa13c406861a37bafca1829a
aaravmithrani <aaravmithrani@ailifebot.in>
2026-02-04 16:03:12 +0530

    extracted phone number and email

--- extract.py
+++ extract.py
@@ -1,9 +1,34 @@
+import re
 import fitz  # PyMuPDF
 
+# 1. Open the PDF and extract text FIRST
 doc = fitz.open("resume.pdf")
 
 text = ""
 for page in doc:
     text += page.get_text()
 
-print(text[:500])
+# 2. Now search inside the text
+email_match = re.search(
+    r"[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}",
+    text
+)
+phone_match = re.search(r"\+\d{1,3}[- ]?\d{10}", text)
+
+if phone_match:
+    phone = phone_match.group()
+else:
+    phone = None
+
+print("Phone:", phone)
+
+
+if email_match:
+    email = email_match.group()
+else:
+    email = None
+
+print("Email:", email)
+
+# Optional: sanity check
+print(text[:300])
