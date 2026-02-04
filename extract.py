0e4592dda42938c382b502250165b0482cc8a62a
aaravmithrani <aaravmithrani@ailifebot.in>
2026-02-04 16:48:41 +0530

    extraction working fine now refining for several for special fields

--- extract.py
+++ extract.py
@@ -1,37 +1,56 @@
-# import re
-# import fitz  # PyMuPDF
+# # import re
+# # import fitz  # PyMuPDF
 
-# # 1. Open the PDF and extract text FIRST
-# doc = fitz.open("resume.pdf")
+# # # 1. Open the PDF and extract text FIRST
+# # doc = fitz.open("resume.pdf")
+
+# # text = ""
+# # for page in doc:
+# #     text += page.get_text()
+
+# # # 2. Now search inside the text
+# # email_match = re.search(
+# #     r"[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}",
+# #     text
+# # )
+# # phone_match = re.search(r"\+\d{1,3}[- ]?\d{10}", text)
+
+# # if phone_match:
+# #     phone = phone_match.group()
+# # else:
+# #     phone = None
+
+# # print("Phone:", phone)
+
+
+# # if email_match:
+# #     email = email_match.group()
+# # else:
+# #     email = None
+
+# # print("Email:", email)
+
+# # # Optional: sanity check
+# # print(text[:300])
+
+
+# import fitz
+
+# doc = fitz.open("policy.pdf")
 
 # text = ""
 # for page in doc:
 #     text += page.get_text()
 
-# # 2. Now search inside the text
-# email_match = re.search(
-#     r"[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}",
-#     text
-# )
-# phone_match = re.search(r"\+\d{1,3}[- ]?\d{10}", text)
-
-# if phone_match:
-#     phone = phone_match.group()
-# else:
-#     phone = None
-
-# print("Phone:", phone)
-
-
-# if email_match:
-#     email = email_match.group()
-# else:
-#     email = None
-
-# print("Email:", email)
-
-# # Optional: sanity check
-# print(text[:300])
+# # print(len(text))
+# for i in range(1, 7):  # pages 2 to 7
+#     page_text = doc[i].get_text().lower()
+#     if any(keyword in page_text for keyword in [
+#         "policy no", "policy number", "premium", "sum insured", "gross premium", "total premium"
+#     ]):
+#         print("Financial data found on page:", i + 1)
+#         print(page_text[:1200])
+#         break
 
 
 import fitz
@@ -42,14 +61,4 @@ text = ""
 for page in doc:
     text += page.get_text()
 
-# print(len(text))
-for i in range(1, 7):  # pages 2 to 7
-    page_text = doc[i].get_text().lower()
-    if any(keyword in page_text for keyword in [
-        "policy no", "policy number", "premium", "sum insured", "gross premium", "total premium"
-    ]):
-        print("Financial data found on page:", i + 1)
-        print(page_text[:1200])
-        break
-
-
+print(text[:1500])
