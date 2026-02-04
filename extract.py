d4fc1def92ef3b957bdc6c841af7c45a8a44791e
aaravmithrani <aaravmithrani@ailifebot.in>
2026-02-04 16:52:30 +0530

    final working code

--- extract.py
+++ extract.py
@@ -1,64 +1,42 @@
-# # import re
-# # import fitz  # PyMuPDF
 
-# # # 1. Open the PDF and extract text FIRST
-# # doc = fitz.open("resume.pdf")
-
-# # text = ""
-# # for page in doc:
-# #     text += page.get_text()
-
-# # # 2. Now search inside the text
-# # email_match = re.search(
-# #     r"[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}",
-# #     text
-# # )
-# # phone_match = re.search(r"\+\d{1,3}[- ]?\d{10}", text)
-
-# # if phone_match:
-# #     phone = phone_match.group()
-# # else:
-# #     phone = None
-
-# # print("Phone:", phone)
-
-
-# # if email_match:
-# #     email = email_match.group()
-# # else:
-# #     email = None
-
-# # print("Email:", email)
-
-# # # Optional: sanity check
-# # print(text[:300])
-
-
-# import fitz
-
-# doc = fitz.open("policy.pdf")
-
-# text = ""
-# for page in doc:
-#     text += page.get_text()
-
-# # print(len(text))
-# for i in range(1, 7):  # pages 2 to 7
-#     page_text = doc[i].get_text().lower()
-#     if any(keyword in page_text for keyword in [
-#         "policy no", "policy number", "premium", "sum insured", "gross premium", "total premium"
-#     ]):
-#         print("Financial data found on page:", i + 1)
-#         print(page_text[:1200])
-#         break
-
-
+import re
 import fitz
 
+# Open PDF
 doc = fitz.open("policy.pdf")
 
+# Extract full text
 text = ""
 for page in doc:
     text += page.get_text()
 
-print(text[:1500])
+# REGEX EXTRACTION 
+
+policy_no = re.search(
+    r"(?<!Prev\s)Policy No\s*:\s*([0-9/]+)",
+    text
+)
+
+
+policy_period = re.search(
+    r"FROM\s+[0-9: ]+ON\s+([0-9/]+)\s+TO\s+MIDNIGHT\s+OF\s+([0-9/]+)",
+    text
+)
+gross_premium = re.search(r"\n\s*([0-9,]+)\s*\n\s*[0-9,]+\s*\n\s*\.5\s*\n\s*[0-9,]+", text)
+gst = re.search(r"\n\s*[0-9,]+\s*\n\s*([0-9,]+)\s*\n\s*\.5", text)
+total_premium = re.search(r"\n\s*[0-9,]+\s*\n\s*[0-9,]+\s*\n\s*\.5\s*\n\s*([0-9,]+)", text)
+
+#STRUCTURED OUTPUT
+
+data = {
+    "policy_number": policy_no.group(1) if policy_no else None,
+    "policy_period": {
+        "from": policy_period.group(1),
+        "to": policy_period.group(2)
+    } if policy_period else None,
+    "gross_premium": gross_premium.group(1).replace(",", "") if gross_premium else None,
+    "gst": gst.group(1).replace(",", "") if gst else None,
+    "total_premium": total_premium.group(1).replace(",", "") if total_premium else None
+}
+
+print(data)
